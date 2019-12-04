c942a2f6699b21592d9d7963655f1960c0300fdc
chris meyers <chris.meyers.fsu@gmail.com>
2019-12-04 06:24:33 -0500

    day 4

---
+++ 2019/4b.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+range_start = 359282
+range_end = 820401
+diff = range_end-range_start
+
+def has_adjacent_digits(num):
+    s = str(num)
+    seq = 0
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            seq += 1
+        else:
+            if seq == 1:
+                return True
+            seq = 0
+    if seq == 1:
+        return True
+
+    return False
+
+def increases_or_same(num):
+    s = str(num)
+    for i in range(len(s)-1):
+        if s[i+1] < s[i]:
+            return False
+    return True
+
+total = 0
+for i in range(range_start, range_end):
+    if has_adjacent_digits(i) and increases_or_same(i):
+        total += 1
+
+assert total < diff
+
+print("Total is {}".format(total))
+
+
