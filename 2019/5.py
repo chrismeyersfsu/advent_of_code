8361d3e1f0dd491f9f0a8071c8a60a5017915b8d
chris meyers <chris.meyers.fsu@gmail.com>
2019-12-07 05:07:54 -0500

    actual day 5

--- 2019/5.py
+++ 2019/5.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python
 
+import sys
+
 def doit():
     with open('5.input', 'r') as f:
-        d = list(f.readline().rstrip().split(','))
+    #with open('5b.input.sample', 'r') as f:
+        d = list(map(f.readline().rstrip().split(','))
+
 
     i = 0
+    step = 0
     while i < len(d):
-        if d[i] == 1:
-            print("add")
-            d[d[i+3]] = d[d[i+1]] + d[d[i+2]]
-        elif d[i] == 2:
-            print("multiply")
-            d[d[i+3]] = d[d[i+1]] * d[d[i+2]]
-        elif d[i] == 99:
-            print("end {}".format(d[0]))
+        inst = d[i]
+        insts = str(inst)
+        op = int(inst) if len(inst) == 1 else (10*int(insts[-2])) + int(insts[-1])
+        poi1 = 0 if len(insts) <= 2 else insts[-3]
+        poi2 = 0 if len(insts) <= 3 else insts[-4]
+        poi3 = 0 if len(insts) <= 4 else insts[-5]
+
+        print("{} {} {} {}".format(poi3, poi2, poi1, op))
+
+        step = 4
+        if op == 1:
+            param1 = d[i+1] if poi1 else d[d[i+1]]
+            param2 = d[i+2] if poi1 else d[d[i+2]]
+            d[d[i+3]] = param1 + param2
+        elif op == 2:
+            param1 = d[i+1] if poi1 else d[d[i+1]]
+            param2 = d[i+2] if poi1 else d[d[i+2]]
+            d[d[i+3]] = param1 * param2
+        elif op == 3:
+            step = 2
+            print("Give me input: ")
+            input = sys.stdin.readline().rstrip()
+            d[d[i+1]] = input
+        elif op == 4:
+            step = 2
+            print("Output: {}".format(d[d[i+1]]))
+        elif op == 99:
             return d
         else:
             print("Blarg! [{}] = {}".format(i, d[i]))
             return d
-        i += 4
+        i += step
     print("WTF? we got to the end of the ticker tape")
 
 d = doit()
